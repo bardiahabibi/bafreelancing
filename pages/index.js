4c4af07b37508937fdb751521ce9348c983552f6
Bardia <bardiafreelancing@gmail.com>
2020-08-22 14:23:40 +0430

    index page google analytics tag added

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,20 @@ class index extends React.Component {
     return (
       <div>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-176061274-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-176061274-1');
+  `,
+            }}
+          />
           <title>BaFreelancing</title>
           <meta
             name="viewport"
